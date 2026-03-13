df6e460598535aa44050562cbf2022f1605a2c62
wei-hsuan-cheng <johnathancheng0125@gmail.com>
2026-03-13 09:15:35 +0800

    Update door pose params

--- launch/door_pose_estimation.launch.py
+++ launch/door_pose_estimation.launch.py
@@ -1,22 +1,30 @@
-from launch import LaunchDescription
-from ament_index_python.packages import get_package_share_directory
-from launch_ros.actions import Node
 import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
 
 
 def generate_launch_description():
     params_file = os.path.join(
         get_package_share_directory("door_pose_estimation"), "config", "door_params.yaml"
     )
+    use_sim_time = ParameterValue(LaunchConfiguration("use_sim_time"), value_type=bool)
+
     return LaunchDescription(
         [
+            DeclareLaunchArgument("use_sim_time", default_value="false"),
             Node(
                 package="door_pose_estimation",
                 executable="door_pose_estimation_action_server",
                 name="door_pose_estimation_action_server",
                 output="screen",
                 parameters=[
-                    params_file
+                    params_file,
+                    {"use_sim_time": use_sim_time},
                 ],
             )
         ]
